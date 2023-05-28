496faacc56b6e252d2249e32012b1d94093844ae
adenteo <adenteo@gmail.com>
2023-05-29 00:30:27 +0800

    Update complexNavbar.js

--- src/app/components/complexNavbar.js
+++ src/app/components/complexNavbar.js
@@ -272,35 +272,4 @@ export default function ComplexNavbar() {
             </Collapse>
         </Navbar>
     );
-  }, []);
-
-  return (
-    <Navbar className="mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6 lg:my-5">
-      <div className="relative mx-auto flex items-center text-blue-gray-900">
-        <Typography
-          as="a"
-          href="#"
-          className="mr-4 ml-2 cursor-pointer py-1.5 font-medium font-montserrat"
-        >
-          WorkJoy
-        </Typography>
-        <div className="absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block">
-          <NavList />
-        </div>
-        <IconButton
-          size="sm"
-          color="blue-gray"
-          variant="text"
-          onClick={toggleIsNavOpen}
-          className="ml-auto mr-2 lg:hidden"
-        >
-          <Bars2Icon className="h-6 w-6" />
-        </IconButton>
-        <ProfileMenu />
-      </div>
-      <Collapse open={isNavOpen} className="overflow-scroll">
-        <NavList />
-      </Collapse>
-    </Navbar>
-  );
 }
