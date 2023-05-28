c6e668fa06e8512d727fe22cf7e35f68c9e8db51
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2023-05-28 22:14:10 +0800

    format code with prettier

--- src/app/components/announcementHeader.js
+++ src/app/components/announcementHeader.js
@@ -1,10 +1,10 @@
-import { HiOutlineBell } from "react-icons/hi";
+import { HiOutlineBell } from 'react-icons/hi';
 
 export default function AnnouncementHeader() {
   return (
-    <div className="bg-cyan-50 text-center p-4 text-gray-800 mt-10 mx-auto w-9/12 border border-gray-200 rounded-xl shadow-md">
-      <HiOutlineBell className="mx-auto text-3xl text-center text-cyan-700 " />
-      <h1 className="font-semibold text-xl p-3">Company retreat</h1>
+    <div className="mx-auto mt-10 w-9/12 rounded-xl border border-gray-200 bg-cyan-50 p-4 text-center text-gray-800 shadow-md">
+      <HiOutlineBell className="mx-auto text-center text-3xl text-cyan-700 " />
+      <h1 className="p-3 text-xl font-semibold">Company retreat</h1>
       <p className="px-5">
         Reminder to sign up for XXX company retreat on 22nd May
       </p>
