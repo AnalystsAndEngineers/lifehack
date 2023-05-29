fa303bf37e888cdc70ae2fffa00669d426d10869
adenteo <adenteo@gmail.com>
2023-05-30 00:27:00 +0800

    Add login page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.scss';
 import { ThemeProvider } from './components/themeprovider';
+import { cookies } from 'next/headers';
+import styles from '../../styles/styles.css';
 
 import { Montserrat } from 'next/font/google';
 import ComplexNavbar from './components/complexNavbar';
@@ -13,15 +15,29 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const sessionId = cookies().get('sessionId')?.value || '';
+  // const sessionId = '';
   return (
     <ThemeProvider>
       <html lang="en">
         <body className={montserrat.className}>
-          <main>
-            <ComplexNavbar />
-            <SgidAuthButton />
-            {children}
-          </main>
+          {sessionId ? (
+            <main>
+              <ComplexNavbar />
+              <SgidAuthButton />
+              {children}
+            </main>
+          ) : (
+            <main className={`flex h-screen items-center justify-center bg-cover bg-image ${styles.bgImage} ${styles.blurBg}`}>
+              <div className="p-4 text-center bg-white rounded-xl p-10">
+                <h1 className="text-xl font-bold">Welcome to WorkJoy</h1>
+                <p className="text-xs">Discover Your joy in work with us.</p>
+                <button className="mx-auto my-5 rounded border border-gray-400 bg-red-400 px-4 py-2 font-semibold text-white shadow hover:bg-red-700">
+                  <a href="/login">Sign in with Singpass</a>
+                </button>
+              </div>
+            </main>
+          )}
         </body>
       </html>
     </ThemeProvider>
