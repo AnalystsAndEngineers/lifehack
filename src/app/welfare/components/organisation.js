bf5e1a9faa0aad5f4517cab59ef5d5fe644019bc
gracelimshiern <98506014+gracelimshiern@users.noreply.github.com>
2023-05-29 21:05:13 +0800

    Edit Welfare page

--- src/app/welfare/components/organisation.js
+++ src/app/welfare/components/organisation.js
@@ -67,42 +67,47 @@ export default function Organisation({ title, desc, imageSrc, routeLink }) {
   ];
 
   return (
-    <div className="lg:w-11/12 w-8/12 md:w-full mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-auto max-h-[70vh]">
-      {data.map((item, index) => (
-        <div className="flex justify-center p-4" key={index}>
-          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-            <div className="p-5">
-              <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
-              <div className=" ">
-                <img src={item.imageSrc} />
-              </div>
+    <div>
+      <div>
+        <h1 className="text-center text-2xl text-cyan-700 font-semibold my-5"> Start Volunteering! </h1>
+      </div>
+      <div className="lg:w-11/12 w-8/12 md:w-full mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-auto max-h-[70vh]">
+        {data.map((item, index) => (
+          <div className="flex justify-center p-4" key={index}>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+              <div className="p-5">
+                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
+                <div className=" ">
+                  <img src={item.imageSrc} />
+                </div>
 
-              <div className="md:h-[16vh] lg:h-[15vh]">
-                <p className="my-3 text-md font-normal text-gray-700 dark:text-gray-400">{item.desc}</p>
-              </div>
-              <a
-                href={item.routeLink}
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  aria-hidden="true"
-                  className="w-4 h-4 ml-2 -mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+                <div className="md:h-[16vh] lg:h-[15vh]">
+                  <p className="my-3 text-md font-normal text-gray-700 dark:text-gray-400">{item.desc}</p>
+                </div>
+                <a
+                  href={item.routeLink}
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </a>
+                  Read more
+                  <svg
+                    aria-hidden="true"
+                    className="w-4 h-4 ml-2 -mr-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
